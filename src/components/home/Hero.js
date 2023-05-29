437ab76f8bf56ad846f983c84cbe90b3cb5d4f04
김영훈 <joseph0926@naver.com>
2023-05-29 20:50:23 +0900

    #16 Hero

---
+++ src/components/home/Hero.js
@@ -0,0 +1,27 @@
+import { Box, Button, Heading } from "@chakra-ui/react";
+
+const Hero = () => {
+  return (
+    <Box as="div">
+      <Box>
+        <Box>
+          <Heading as="h1">asdf</Heading>
+          <Text>asdasdasdasdasdasdasdasd</Text>
+        </Box>
+        <Box>
+          <Button>
+            Start
+            {/* Img */}
+          </Button>
+          <Button>
+            Contact
+            {/* Img */}
+          </Button>
+        </Box>
+        <Box>{/* Img */}</Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Hero;
