2c1123fb8647f6ca2761adc959ae7c1d4bbfe8ae
김영훈 <joseph0926@naver.com>
2023-06-01 10:11:13 +0900

    #21 Hero fix

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -7,10 +7,10 @@ const Hero = () => {
     <Flex w="100vw" h="70vh" justify="center" align="center" position="relative">
       <Flex direction={{ base: "column", md: "row" }} justify="flex-start" alignItems="center" gap={10}>
         <Box w="50%">
-          <Heading mb={4}>Payments infrastructure for the world</Heading>
+          <Heading mb={4}>Barcelona Fan Community Platform</Heading>
           <Text mb={4}>
-            Tons of companies all over the world use Swipe’s seamless platform to manage their businesses's entire financial transaction
-            infrastructure online.
+            FC Barcelona 팬 커뮤니티 플랫폼입니다. 이 플랫폼은 팬들이 모여 소통하고 정보를 공유하며, 함께 커뮤니티를 형성할 수 있는 온라인
+            공간을 제공합니다. 다양한 정보와 소식을 공유해보세요!
           </Text>
           <Flex mt={4}>
             <Button>Start now</Button>
