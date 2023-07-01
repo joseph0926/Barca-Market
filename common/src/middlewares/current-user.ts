de5c81641bff252ac9ad6b27b7b4da2e0bd89d5c
joseph0926 <joseph0926@naver.com>
2023-07-02 07:41:10 +0900

    Updates

---
+++ common/src/middlewares/current-user.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+
+export const currentUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.session?.jwt) {
+    return next();
+  }
+
+  try {
+    const payload = jwt.verify(
+      req.session.jwt,
+      process.env.JWT_SECRET!
+    ) as UserPayload;
+    req.currentUser = payload;
+  } catch (error) {
+    console.log(error);
+  }
+  next();
+};
