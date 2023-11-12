5e9057e7084396f0492c3cba596769ac04cbd514
김영훈 <joseph0926@naver.com>
2023-11-12 11:28:50 +0900

    #65 auth 서비스 수정

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,36 +1,70 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
+import express, {
+  NextFunction,
+  Request,
+  Response,
+  json,
+  urlencoded,
+} from "express";
+import cors from "cors";
+import helmet from "helmet";
+import hpp from "hpp";
+import compression from "compression";
 import cookieSession from "cookie-session";
+import { config } from "@/config";
+import { CustomError, IErrorResponse } from "@/libs/error-handler";
 
-import { currentUserRouter } from "./routes/current-user";
-import { signupRouter } from "./routes/signup";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { verifyEmailRouter } from "./routes/verify-email";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
+const PORT = config.DEV_SERVER_PORT!;
 const app = express();
-app.set("trust proxy", true);
-app.use(json());
+
+// protected-middleware
 app.use(
   cookieSession({
-    signed: false,
-    secure: process.env.NODE_ENV !== "test",
+    name: "seesion",
+    keys: ["secret1", "secret2"],
+    maxAge: 1 * 24 * 60 * 60 * 1000,
+    secure: false, // 변경
+  })
+);
+app.use(hpp());
+app.use(helmet());
+app.use(
+  cors({
+    origin: config.DEV_CLIENT_URL!,
+    credentials: true,
+    optionsSuccessStatus: 200,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   })
 );
 
-app.use(signupRouter);
-app.use(verifyEmailRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(currentUserRouter);
+// standard-middlware
+app.use(compression());
+app.use(json({ limit: "50mb" }));
+app.use(urlencoded({ extended: true, limit: "50mb" }));
 
-app.get("*", async () => {
-  throw new NotFoundError();
+// route-middleware
+
+// error-middleware
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  res
+    .status(404)
+    .json({ message: `${req.originalUrl}에 대한 요청은 잘못된 요청입니다.` });
+
+  app.use(
+    (
+      error: IErrorResponse,
+      req: Request,
+      res: Response,
+      next: NextFunction
+    ) => {
+      console.log(error);
+      if (error instanceof CustomError) {
+        return res.status(error.statusCode).json(error.serializeErrors());
+      }
+      next();
+    }
+  );
 });
 
-app.use(errorHandler);
-
-export default app;
+app.listen(PORT, () => {
+  console.log(`서버가 포트번호 ${PORT}에서 정상작동중입니다.`);
+});
