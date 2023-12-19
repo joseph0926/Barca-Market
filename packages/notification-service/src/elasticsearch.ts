4259d199b62496eeb355a0f59c81c8a58da0f90d
김영훈 <joseph0926@naver.com>
2023-12-19 18:37:02 +0900

    #85 notification service

---
+++ packages/notification-service/src/elasticsearch.ts
@@ -0,0 +1,33 @@
+import { Client } from '@elastic/elasticsearch';
+import { ClusterHealthResponse } from '@elastic/elasticsearch/lib/api/types';
+import { Logger } from 'winston';
+import { config } from '@notification/config';
+import { winstonLogger } from '@base/logger';
+
+const log: Logger = winstonLogger(
+  `${config.ELASTIC_SEARCH_URL}`,
+  'notificationElasticSearchServer',
+  'debug',
+);
+
+const elasticSearchClient = new Client({
+  node: `http://localhost:9200`,
+});
+
+export const checkConnection = async (): Promise<void> => {
+  let isConnected = false;
+
+  while (!isConnected) {
+    try {
+      const health: ClusterHealthResponse =
+        await elasticSearchClient.cluster.health({});
+      log.info(
+        `NotificationService ElasticSearch health status - ${health.status}`,
+      );
+      isConnected = true;
+    } catch (error) {
+      log.error('Connection to ElasticSearch Failed,,, Retrying...');
+      log.log('error', 'NotificationServer checkConnection() method: ', error);
+    }
+  }
+};
