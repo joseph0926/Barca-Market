9ca3d1ad54cc970cb106057507d8ca45d8559137
김영훈 <joseph0926@naver.com>
2023-12-20 15:25:25 +0900

    #93 GatewayService

---
+++ packages/gateway-service/jest.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from '@jest/types';
+
+const jestConfig: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  coverageDirectory: 'coverage',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/test/*.ts?(x)',
+    '!**/node_modules/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 1,
+      functions: 1,
+      lines: 1,
+      statements: 1,
+    },
+  },
+  coverageReporters: ['text-summary', 'lcov'],
+  testPathIgnorePatterns: ['/node_modules'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  testMatch: ['<rootDir>/src/**/test/*.ts'],
+  moduleNameMapper: {
+    '@gateway/(.*)': ['<rootDir>/src/$1'],
+    '@base/(.*)': ['<rootDir>/../../src/$1'],
+  },
+};
+
+export default jestConfig;
---
+++ packages/gateway-service/src/server.ts
@@ -0,0 +1,127 @@
+import http from 'http';
+import { Logger } from 'winston';
+import {
+  Application,
+  NextFunction,
+  Request,
+  Response,
+  json,
+  urlencoded,
+} from 'express';
+import cookieSession from 'cookie-session';
+import hpp from 'hpp';
+import cors from 'cors';
+import helmet from 'helmet';
+import compression from 'compression';
+import { StatusCodes } from 'http-status-codes';
+import { winstonLogger } from '@base/logger';
+import { CustomError, IErrorResponse } from '@base/custom-error-handler';
+import { config } from '@gateway/config';
+import { elasticSearch } from '@gateway/elasticsearch';
+import { appRoutes } from '@gateway/routes';
+
+const SERVER_PORT = 4000;
+const log: Logger = winstonLogger(
+  `${config.ELASTIC_SEARCH_URL}`,
+  'apiGatewayServer',
+  'debug',
+);
+
+export class GatewayServer {
+  private app: Application;
+
+  constructor(app: Application) {
+    this.app = app;
+  }
+
+  public start(): void {
+    this.securityMiddleware(this.app);
+    this.standardMiddleware(this.app);
+    this.routesMiddleware(this.app);
+    this.startElasticSearch();
+    this.errorHandler(this.app);
+    this.startServer(this.app);
+  }
+
+  private securityMiddleware(app: Application): void {
+    app.set('trust proxy', 1);
+    app.use(
+      cookieSession({
+        name: 'session',
+        keys: [`${config.SECRET_KEY_ONE}`, `${config.SECRET_KEY_TWO}`],
+        maxAge: 7 * 24 * 1000 * 3600,
+        secure: config.NODE_ENV !== 'development',
+      }),
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        origin: config.CLIENT_URL,
+        credentials: true,
+        methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],
+      }),
+    );
+  }
+
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: '200mb' }));
+    app.use(urlencoded({ limit: '200mb', extended: true }));
+  }
+
+  private routesMiddleware(app: Application): void {
+    appRoutes(app);
+  }
+
+  private startElasticSearch(): void {
+    elasticSearch.checkConnection();
+  }
+
+  private errorHandler(app: Application): void {
+    app.use('*', (req: Request, res: Response, next: NextFunction) => {
+      const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+      log.log('error', `That ${fullUrl} endpoint doesn not exist.`, '');
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: 'That API endpoint doesn not exist.' });
+      next();
+    });
+    app.use(
+      (
+        error: IErrorResponse,
+        _req: Request,
+        res: Response,
+        next: NextFunction,
+      ) => {
+        log.log('error', `GatewayService ${error.comingFrom}: `, error);
+        if (error instanceof CustomError) {
+          res.status(error.statusCode).json(error.serializeError());
+        }
+        next();
+      },
+    );
+  }
+
+  private async startServer(app: Application): Promise<void> {
+    try {
+      const httpServer: http.Server = new http.Server(app);
+      this.startHttpServer(httpServer);
+    } catch (error) {
+      log.log('error', `GatewayService startServer() method: `, error);
+    }
+  }
+
+  private async startHttpServer(httpServer: http.Server): Promise<void> {
+    try {
+      log.info(
+        `Worker with process id of ${process.pid} on gateway server has started`,
+      );
+      httpServer.listen(SERVER_PORT, () => {
+        log.info(`GatewayServer is running on port number ${SERVER_PORT}.`);
+      });
+    } catch (error) {
+      log.log('error', `GatewayService startHttpServer() method: `, error);
+    }
+  }
+}
